518e439b91197036b329963cd989d3fcba09ad87
scj040921 <2465618779@qq.com>
2026-07-18 16:46:02 +0800

    vault backup: 2026-07-18 16:46:02

--- c_language/function_demo_01.c
+++ c_language/function_demo_01.c
@@ -1,24 +1,5 @@
 #include<stdio.h>
-#include<stdbool.h>
-bool judge(int a)
-{
-     if(a==2)
-        {
-            return true;
-        }
-        if(a==1)
-        {
-            return false;
-        }
-    for(int i=2;i*i<=a;i++)
-    {
-        if(a%i==0)
-        {
-            return false;
-        }
-    }
-    return true;
-}
+#include"head.h"
 int main()
 {
     int a;
